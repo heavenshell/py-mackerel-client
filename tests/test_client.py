496b3c516d2d959b533007e574ac8ed948ffd990
heavenshell <heavenshell.jp@gmail.com>
2015-03-02 20:02:20 +0900

    Add tests and requirements.txt

---
+++ tests/test_client.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+    mackerel.tests.test_client
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    Mackerel client tests.
+
+
+    :copyright: (c) 2015 Shinya Ohyanagi, All rights reserved.
+    :license: BSD, see LICENSE for more details.
+"""
+import os
+from unittest import TestCase
+from mackerel.client import Client
+from mackerel.host import Host
+
+
+class TestClient(TestCase):
+    @classmethod
+    def setUpClass(cls):
+        api_key = os.environ.get('MACKEREL_API_KEY')
+        cls.client = Client(mackerel_api_key=api_key)
+
+    def test_should_get_hosts(self):
+        """ Client().get_hosts() should get host list. """
+        hosts = self.client.get_hosts()
+        for host in hosts:
+            self.assertTrue(isinstance(host, Host))
