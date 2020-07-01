b33de90d31e8d84f55eab6fcddae1d789aa85f9d
Shinya Ohyanagi <heavenshell.jp@gmail.com>
2020-07-02 00:47:53 +0900

    Release 0.2.1 (#43)

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ with open(rst_path) as f:
 
 setup(
     name=app_name,
-    version='0.2.0',
+    version='0.2.1',
     author='Shinya Ohyanagi',
     author_email='sohyanagi@gmail.com',
     url='http://github.com/heavenshell/py-mackerel-client',
